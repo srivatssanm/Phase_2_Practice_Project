b53fb459bafbff56b1109c898615c0d1e795b850
Srivatssan <94544700+srivatssanm@users.noreply.github.com>
2022-08-03 14:31:19 +0530

    Phase2_PracticeProject_1

---
+++ ValidationOfUserLogin/src/main/java/com/businesslogic/LoginChecker.java
@@ -0,0 +1,62 @@
+package com.businesslogic;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/LoginChecker")
+public class LoginChecker extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public LoginChecker() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		String uname=request.getParameter("txtUname");
+		String pword=request.getParameter("txtPassword");
+		PrintWriter out=response.getWriter();
+		if(uname.equalsIgnoreCase("srivatssan") && pword.equals("phase2")){
+			out.println("You are Welcomed");
+		}
+		else
+			out.println("Invalid Username or Password");
+		
+	}
+
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		String uname=request.getParameter("txtUname");
+		String pword=request.getParameter("txtPassword");
+		PrintWriter out=response.getWriter();
+		//RequestDispatcher- an interface to include content on same page or different pages
+		RequestDispatcher rd;
+		if(uname.equalsIgnoreCase("srivatssan") && pword.equals("phase2")){
+			//session object is created bby web conatiner and generate sessionid
+			HttpSession session=request.getSession(true);//create a new object and store its reference in session object
+			session.setAttribute("username", uname);
+			
+			rd=request.getRequestDispatcher("Welcome");
+			rd.forward(request, response);
+		}
+		else {
+			out.println("Invalid Username or Password");
+			rd=request.getRequestDispatcher("LoginForm.html");
+			rd.include(request, response);
+		}
+	}
+}
